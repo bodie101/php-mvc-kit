a402c40286598f9d40511cbe10dc72e44014f910
Jeffrey Kranenburg <jeff.kranenburg@gmail.com>
2018-06-13 20:32:12 +1200

    Initial Commit

---
+++ app/helper/config.php
@@ -0,0 +1,12 @@
+<?php
+
+    define ('APPROOT', dirname(dirname(__FILE__)));
+    define ('URLROOT', "http://localhost:8000/");
+    define ('SITENAME', "SOME MVC SITE");
+
+    define ("DBHOST", "phpmvckit_db_1");
+    define ("DBUSER", "root");
+    define ("DBPASS", "(password123)");
+    define ("DBNAME", "containerdb");
+
+?>
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Basic MVC Framework 
+
+This is the framework to be used for your assignment.
+
+This MVC framework is written in PHP and is based on an opensource framework by [Brad Traversy](http://traversymedia.com) 
+
