ad7f17e19eb861b13db56d2136e458ebfaeb3071
Jeffrey Kranenburg <jeffrey.kranenburg@toiohomai.ac.nz>
2018-10-15 14:14:44 +1300

    Update startup.php

--- app/helper/startup.php
+++ app/helper/startup.php
@@ -1,7 +1,16 @@
 <?php
 
-  require_once(__DIR__.'/vendor/autoload.php');
-  $dotenv = new Dotenv\Dotenv(__DIR__,'/.env');
-  $dotenv->load();
+//  require_once(__DIR__.'/vendor/autoload.php');
+//  $dotenv = new Dotenv\Dotenv(__DIR__,'/.env');
+//  $dotenv->load();
+
+    define ('APPROOT', dirname(dirname(__FILE__)));
+    define ('URLROOT', "http://localhost:8000/");
+    define ('SITENAME', "SOME MVC SITE");
+
+    define ("DBHOST", "phpweb101_db_1");
+    define ("DBUSER", "user");
+    define ("DBPASS", "user1234");
+    define ("DBNAME", "containerdb");
 
 ?>
