be1d311ed2d2be392d184614759ca5151e621b2f
Jeffrey Kranenburg <jeff.kranenburg@gmail.com>
2018-10-16 09:29:04 +1300

    Streamline

--- app/helper/startup.php
+++ app/helper/startup.php
@@ -1,9 +1,5 @@
 <?php
 
-//  require_once(__DIR__.'/vendor/autoload.php');
-//  $dotenv = new Dotenv\Dotenv(__DIR__,'/.env');
-//  $dotenv->load();
-
     define ('APPROOT', dirname(dirname(__FILE__)));
     define ('URLROOT', "http://localhost:8000/");
     define ('SITENAME', "SOME MVC SITE");
